d937e976e703aaf1e5b0a5b508afa2751f0a8031
Maxim <mgerasimchik2018@gmail.com>
2022-10-30 01:01:44 +0300

    offer list and comment form

---
+++ project/src/components/offers-list/offers-list.tsx
@@ -0,0 +1,20 @@
+import PlaceCard from '../place-card/place-card';
+import {Offers} from '../../types/offer';
+
+type OffersListProps = {
+  offers: Offers;
+  handleCardOver: (id: number) => void;
+  handleCardOut: () => void;
+}
+
+function OffersList({offers, handleCardOver, handleCardOut}: OffersListProps): JSX.Element {
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {offers.map((offer) => (
+        <PlaceCard handleCardOver={(id: number)=>handleCardOver(id)} handleCardOut={handleCardOut} key={offer.id} offer={offer}/>
+      ))}
+    </div>
+  );
+}
+
+export default OffersList;
--- project/src/components/place-card/place-card.tsx
+++ project/src/components/place-card/place-card.tsx
@@ -1,28 +1,45 @@
-function PlaceCard(): JSX.Element {
+import {Offer} from '../../types/offer';
+import {Link} from 'react-router-dom';
+
+type OfferProps = {
+  offer: Offer;
+  handleCardOver: (id: number) => void;
+  handleCardOut: () => void;
+}
+
+function PlaceCard({offer, handleCardOver, handleCardOut}: OfferProps): JSX.Element {
+  const ratingPercent: number = offer.rating * 20;
+
   return (
-    <article className="cities__card place-card">
+    <article className="cities__card place-card" onMouseEnter={() => handleCardOver(offer.id)} onMouseLeave={handleCardOut}>
+      {
+        offer.premium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      }
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
-        </a>
+        <Link to={`/offer/${offer.id}`}>
+          <img className="place-card__image" src={offer.mainPicture.src} width="260" height="200" alt="Place image"/>
+        </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;80</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
+            <b className="place-card__price-value">&euro;{offer.price.cost}</b>
+            <span className="place-card__price-text">&#47;&nbsp;{offer.price.period}</span>
           </div>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: '80%'}}></span>
+            <span style={{width: `${ratingPercent}%`}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Wood and stone place</a>
+          <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
         </h2>
-        <p className="place-card__type">Private room</p>
+        <p className="place-card__type">{offer.type}</p>
       </div>
     </article>
   );
